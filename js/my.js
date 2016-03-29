0d2533bc73a362f8a83bd446a36b1ac470bc3f83
darinawinner <darinakostina94@gmail.com>
2016-03-29 20:16:48 +0300

    #1 частично #2 только размер #3 сделала #4 сделала #5 нет #6 нет

---
+++ js/my.js
@@ -0,0 +1,31 @@
+ $("#maininfo1").on('inview', function (event, isInView) {
+    if (isInView) {
+        $("#maininfo1").css({opacity:  "1"});
+        }
+   });
+
+ $("#maininfo2").on('inview', function (event, isInView) {
+    if (isInView) {
+        $("#maininfo2").css({opacity:  "1"});
+        }
+   });
+
+ $("#maininfo3").on('inview', function (event, isInView) {
+    if (isInView) {
+        $("#maininfo3").css({opacity:  "1"});
+        }
+   });
+
+/*function setheight(){ 
+var theWindow = $(window).height(); 
+$('#header').height(theWindow); 
+} 
+
+$(window).load(function() { 
+setheight(); 
+}); 
+
+$(window).resize(function(){ 
+setheight(); 
+});*/
+
