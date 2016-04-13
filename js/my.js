910cbd83f0f601f43280a1b7292a894f041f1e6a
darinawinner <darinakostina94@gmail.com>
2016-04-13 17:25:51 +0300

    #3 + #4 +
    все работает кроме скрипта подстраивающего первый слайд под высоту
    браузера (он работает неправильно ничего не могу с этим сделать)

--- js/my.js
+++ js/my.js
@@ -1,3 +1,4 @@
+//прозрачность блоков
 $("#maininfo1").on('inview', function (event, isInView) {
     if (isInView) {
         $("#maininfo1").css({opacity:  "1"});
@@ -16,17 +17,59 @@ $("#maininfo1").on('inview', function (event, isInView) {
         }
    });
 
+//затемнение первого блока
+$(window).scroll(function() {
+var top = $(document).scrollTop();
+if (top > 100) $('#header').addClass('shadow1');//200 - это значение высоты прокрутки страницы для добавления класс
+else $('#header').removeClass('shadow1');
+    if (top > 150) $('#header').addClass('shadow2'); //200 - это значение высоты прокрутки страницы для добавления класс
+else $('#header').removeClass('shadow2');
+    if (top > 200) $('#header').addClass('shadow3'); //200 - это значение высоты прокрутки страницы для добавления класс
+else $('#header').removeClass('shadow3');
+    if (top > 250) $('#header').addClass('shadow4'); //200 - это значение высоты прокрутки страницы для добавления класс
+else $('#header').removeClass('shadow4');
+    if (top > 300) $('#header').addClass('shadow5'); //200 - это значение высоты прокрутки страницы для добавления класс
+else $('#header').removeClass('shadow5');
+    //alert ('ghjkl');
+});
 
-/**function setheight(){ 
-var theWindow = $(window).height(); 
-$('#header').height(theWindow); 
-} 
+//плавная прокрутка
+$('a[href^="#"]').click(function(){
+//Сохраняем значение атрибута href в переменной:
+var target = $(this).attr('href');
+$('html, body').animate({scrollTop: $(target).offset().top}, 800);
+return false;
+});
 
-$(window).load(function() { 
-setheight(); 
-}); 
+//плавная прокрутка и маркеры для меню
+function marker(obj){
+var id = obj.id;
+id = id.substring(0, id.length - 1);
+//Сохраняем значение атрибута href в переменной:
+$('html, body').animate({scrollTop: $('#' + id).offset().top}, 800);
+}
 
-$(window).resize(function(){ 
-setheight(); 
-});**/
+//высота первого блока подстраивается под окно браузера
+/**function setHeiHeight() {
+    $('#header').css({
+        height: $(window).height() + 'px';
+        alert(height);
+    });
+}
+setHeiHeight(); // устанавливаем высоту окна при первой загрузке страницы
+$(window).resize( setHeiHeight ); // обновляем при изменении размеров окна
+**/
 
+/**function setheight(){
+    var theWindow = $(window).height();
+    $('#header').height(theWindow);
+    //alert("monitorHeight: " + theWindow);
+    }
+
+    $(window).load(function() {
+    setheight();
+    });
+
+    $(window).resize(function(){
+    setheight();
+    });**/
